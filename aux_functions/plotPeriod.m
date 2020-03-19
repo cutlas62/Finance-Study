c96cb510ddef1f22c79afc01f3af173cd51488e3
Carlos <carlos.santosr@ieee.org>
2020-03-18 20:30:27 -0400

    Add plotPeriod function

---
+++ aux_functions/plotPeriod.m
@@ -0,0 +1,27 @@
+function plotPeriod(data, dStart, dEnd)
+%plotPeriod(data, dStart, dEnd)
+%
+%   Plot a period between 'dStart' and 'dEnd'.
+%   Both dates have to be in "yyyy/mm/dd" format.
+%
+%   plotPeriod(gspc,"2010/1/31","2015,12,10")
+
+    if ~exist('data', 'var')
+        fprintf('\tERROR: %s does not exist. Did you load it first?\n', data)
+        fprintf('\ttype ''help plotPeriod'' for more information\n')
+        return
+    end
+    
+    startDate = datetime(dStart, 'Inputformat', 'yyyy/MM/dd');
+    endDate = datetime(dEnd, 'InputFormat', 'yyyy/MM/dd');
+    
+    if startDate > endDate
+       fprintf('\tERROR: Start date must be before End date\n')
+       fprintf('\ttype ''help plotPeriod'' for more information\n')
+       return
+    end
+    
+    dateRange = (data.Date > startDate) & (data.Date < endDate);
+    
+    plot(data.Date(dateRange), data.Open(dateRange))
+end
